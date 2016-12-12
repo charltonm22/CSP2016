1e2bfdd9b92fb6b4a1b442ce9a2ddf705f691df7
JCopp <JEFFREY.COPPERTHITE@new-haven.k12.ct.us>
2016-12-12 11:09:03 -0500

    Team name change
    Updated the test team name to "Test Class Example"

--- team14.py
+++ team14.py
@@ -6,7 +6,7 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'haha' # Only 10 chars displayed.
+team_name = 'Test class example' # Only 10 chars displayed.
 strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
